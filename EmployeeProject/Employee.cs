32a9bce6261d6a33cb88c1618a0894b10626d0af
ajitdikle <Ajitdikle@1020>
2022-12-26 18:17:31 +0530

    CSharp properties

--- EmployeeProject/Employee.cs
+++ EmployeeProject/Employee.cs
@@ -10,19 +10,35 @@ namespace EmployeeProject
     {
         public int empId;
         public string empName;
-        public double empSalary;
+        private double _empSalary;
         public char empperformaceType;
         public static string companyName;
         public static string companyLocation;
 
+       
         public void PrintEmployeeDetail()
         { Console.WriteLine("Employee Id:" + empId);
             Console.WriteLine("Employee Name:" + empName);
-            Console.WriteLine("Employee Salary:" + empSalary);
+            Console.WriteLine("Employee Salary:" + _empSalary);
             Console.WriteLine("Employee performace :" + empperformaceType);
             Console.WriteLine("Company Name:" + Employee.companyName);
             Console.WriteLine("Company location:" + Employee.companyLocation);
             Console.WriteLine("_________________________________________________");
+        }
+        public double EmpSalary
+        {
+            get { return _empSalary; }
+            set
+            {
+                if (value > 3000)
+                {
+                    _empSalary = value;
+                }
+                else
+                {
+                    Console.WriteLine("invalid salary");
+                }
+            }
         }
         public void GetGrossSalaryWithBonus()
         {
@@ -30,17 +46,17 @@ namespace EmployeeProject
             if (empperformaceType == 'a')
             {
                 Console.WriteLine("10%");
-                Console.WriteLine(empSalary + (empSalary * 10 / 100));
+                Console.WriteLine(_empSalary + (_empSalary * 10 / 100));
             }
             else if (empperformaceType == 'b')  
                 {
                 Console.WriteLine("20%");
-                Console.WriteLine(empSalary + (empSalary * 20 / 100));
+                Console.WriteLine(_empSalary + (_empSalary * 20 / 100));
                 }
                 else if (empperformaceType=='c')
                 {
                 Console.WriteLine("30%");
-                Console.WriteLine(empSalary + (empSalary * 30 / 100));
+                Console.WriteLine(_empSalary + (_empSalary * 30 / 100));
 
             }
             else
--- EmployeeProject/Program.cs
+++ EmployeeProject/Program.cs
@@ -16,25 +16,28 @@ namespace Maveric.Runner
 
             emp1.empId = 101;
             emp1.empName = "saul";
-            emp1.empSalary = 9000;
+            //emp1.empSalary = 9000;
             emp1.empperformaceType = 'a';
+            emp1.EmpSalary = 9000;
 
             emp2.empId = 102;
             emp2.empName = "kim";
-            emp2.empSalary = 1200.2;
+            //emp2.empSalary = 1200.2;
             emp2.empperformaceType = 'b';
+            emp2.EmpSalary = 1200.2;
 
             emp3.empId = 103;
             emp3.empName = "jack";
-            emp3.empSalary = 6000.2;
+            //emp3.empSalary = 6000.2;
             emp3.empperformaceType = 'c';
+            emp3.EmpSalary = -6000.2;
 
             Console.WriteLine(emp1.empId);
             Console.WriteLine(emp1.empName);
-            Console.WriteLine(emp1.empSalary);
+            Console.WriteLine(emp1.EmpSalary);
             Console.WriteLine(emp2.empId);
             Console.WriteLine(emp2.empName);
-            Console.WriteLine(emp2.empSalary);
+            Console.WriteLine(emp2.EmpSalary);
 
             emp2.PrintEmployeeDetail();
             emp1.PrintEmployeeDetail();
