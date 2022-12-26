23b328219ba026a9bd1ea781ec9ffe91508cbe8b
ajitdikle <Ajitdikle@1020>
2022-12-26 17:28:30 +0530

    adding bonus

--- EmployeeProject/Employee.cs
+++ EmployeeProject/Employee.cs
@@ -11,6 +11,44 @@ namespace EmployeeProject
         public int empId;
         public string empName;
         public double empSalary;
+        public char empperformaceType;
         public static string companyName;
+        public static string companyLocation;
+
+        public void PrintEmployeeDetail()
+        { Console.WriteLine("Employee Id:" + empId);
+            Console.WriteLine("Employee Name:" + empName);
+            Console.WriteLine("Employee Salary:" + empSalary);
+            Console.WriteLine("Employee performace :" + empperformaceType);
+            Console.WriteLine("Company Name:" + Employee.companyName);
+            Console.WriteLine("Company location:" + Employee.companyLocation);
+            Console.WriteLine("_________________________________________________");
+        }
+        public void GetGrossSalaryWithBonus()
+        {
+            Console.WriteLine("Employee Id :" + empId);
+            if (empperformaceType == 'a')
+            {
+                Console.WriteLine("10%");
+                Console.WriteLine(empSalary + (empSalary * 10 / 100));
+            }
+            else if (empperformaceType == 'b')  
+                {
+                Console.WriteLine("20%");
+                Console.WriteLine(empSalary + (empSalary * 20 / 100));
+                }
+                else if (empperformaceType=='c')
+                {
+                Console.WriteLine("30%");
+                Console.WriteLine(empSalary + (empSalary * 30 / 100));
+
+            }
+            else
+            {
+                Console.WriteLine("Not Eligble for Bonus");
+            }
+            }
+        }
+
     }
-}
+
--- EmployeeProject/Program.cs
+++ EmployeeProject/Program.cs
@@ -9,7 +9,7 @@ namespace Maveric.Runner
         { //
             Console.WriteLine("employee Dashboard");
             Employee.companyName = "Maveric";
-
+            Employee.companyLocation = "pune";
             Employee emp1 = new Employee();
             Employee emp2 = new Employee();
             Employee emp3 = new Employee();
@@ -17,14 +17,17 @@ namespace Maveric.Runner
             emp1.empId = 101;
             emp1.empName = "saul";
             emp1.empSalary = 9000;
+            emp1.empperformaceType = 'a';
 
             emp2.empId = 102;
             emp2.empName = "kim";
             emp2.empSalary = 1200.2;
+            emp2.empperformaceType = 'b';
 
             emp3.empId = 103;
             emp3.empName = "jack";
             emp3.empSalary = 6000.2;
+            emp3.empperformaceType = 'c';
 
             Console.WriteLine(emp1.empId);
             Console.WriteLine(emp1.empName);
@@ -32,8 +35,14 @@ namespace Maveric.Runner
             Console.WriteLine(emp2.empId);
             Console.WriteLine(emp2.empName);
             Console.WriteLine(emp2.empSalary);
-           
 
+            emp2.PrintEmployeeDetail();
+            emp1.PrintEmployeeDetail();
+            emp3.PrintEmployeeDetail();
+
+            emp1.GetGrossSalaryWithBonus();
+            emp2.GetGrossSalaryWithBonus();
+            emp3.GetGrossSalaryWithBonus();
         }
     }
 }
